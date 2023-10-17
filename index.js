769b25bbafc75c1807dae40daf5c5b2700f09095
Gunjan-Programmer <88590239+Gunjan-Programmer@users.noreply.github.com>
2023-10-17 23:30:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var randomNumber1 = Math.floor(Math.random() * 6 ) + 1;
-console.log("Person1 : " + randomNumber1);
-document.querySelector(".dice .img1").setAttribute("src" , "./dice" + randomNumber1 + ".png");
-
 var randomNumber2 = Math.floor(Math.random() * 6 ) + 1;
-console.log("Person2 : " + randomNumber2);
+
+document.querySelector(".dice .img1").setAttribute("src" , "./dice" + randomNumber1 + ".png");
 document.querySelector(".dice .img2").setAttribute("src" , "./dice" + randomNumber2 + ".png");
+
+console.log("Person1 : " + randomNumber1);
+console.log("Person2 : " + randomNumber2);
+
 
 
 if (randomNumber1 === randomNumber2){
     document.querySelector("h1").textContent = "Winner!";
-    
+    document.querySelector(".body").style.backgroundColor = "green";
     document.querySelector("h1").style.color = "yellow";
 
 }
